98134bc950f7ee4b6334ee275537986628a6e0b4
Kareem Gan <kareemgan@gmail.com>
2016-01-06 23:00:59 -0600

    Fix macro for skipping unmet conditions
    Didn't see this when I pushed this earlier, my bad.

--- spec/lib/poro_validator/validators/inclusion_validator_spec.rb
+++ spec/lib/poro_validator/validators/inclusion_validator_spec.rb
@@ -9,34 +9,35 @@ RSpec.describe PoroValidator::Validators::InclusionValidator do
         include PoroValidator.validator
 
         validates :amount, inclusion: 1..10
+        validates :rate, inclusion: 1..10, if: proc { false }
       end.new
     end
 
     expect_validator_to_be_invalid do
       let(:entity) do
         OpenStruct.new(
-          amount: 11
+          amount: 11,
+          rate: 11
         )
       end
 
       let(:expected_errors) do
         {
-          "amount" => ["is not within the range of 1..10"],
+          "amount" => ["is not within the range of 1..10"]
         }
       end
 
       skip_attr_unmet_condition do
-        let(:attr) { :dob }
+        let(:attr) { :rate }
       end
     end
 
     expect_validator_to_be_valid do
       let(:entity) do
         OpenStruct.new(
-          amount: 5,
+          amount: 5
         )
       end
     end
   end
-
 end
