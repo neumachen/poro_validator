fb95a6fdce60748b1bde0450bf13651d9fe8bbdf
Kareem Gan <kareemgan@gmail.com>
2016-01-07 21:16:36 -0600

    Add length validator

---
+++ lib/poro_validator/validators/length_validator.rb
@@ -0,0 +1,42 @@
+module PoroValidator
+  module Validators
+    class LengthValidator < BaseClass
+      def validate(attribute, value, options)
+        message = options.fetch(:message, :length)
+        on = options.fetch(:on, attribute)
+
+        options.keys.each do |key|
+          matcher = matchers(key)
+          next if matcher.nil?
+          v = value.is_a?(::String) ? value : value.to_s
+          unless matchers(key).call(v, options[key])
+            errors.add(on, message, key => options[key])
+          end
+        end
+      end
+
+      private
+
+      def matchers(key)
+        m = {
+          extremum: lambda do |value, length|
+            value.length == length
+          end,
+
+          max: lambda do |value, length|
+            value.length <= length
+          end,
+
+          min: lambda do |value, length|
+            value.length >= length
+          end,
+
+          in: lambda do |value, length|
+            length.cover?(value.length)
+          end
+        }
+        m.fetch(key, nil)
+      end
+    end
+  end
+end
