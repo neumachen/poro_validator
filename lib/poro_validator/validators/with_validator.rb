9163fbb5df9e9bc6f9995e630bbf98be8c487d4b
Kareem Gan <kareemgan@gmail.com>
2016-01-22 09:44:13 -0600

    fixes #32
    Fix bug where when `with` validator borks when used on a non hash
    or non nested object.

--- lib/poro_validator/validators/with_validator.rb
+++ lib/poro_validator/validators/with_validator.rb
@@ -6,13 +6,30 @@ module PoroValidator
         case validator_class
         when Class
           klass = validator_class.new
+
           if context.entity.is_a?(::Hash)
-            klass.valid?(context.entity[attribute] || {})
+            entity = context.entity[attribute]
+
+            if entity.nil? || entity.is_a?(::Symbol) || entity.is_a?(::String)
+              klass.valid?(attribute => entity)
+            else
+              klass.valid?(context.entity[attribute] || {})
+            end
           else
-            klass.valid?(context.entity.public_send(attribute))
+            entity = context.entity.public_send(attribute)
+            if entity.nil? || entity.is_a?(::Symbol) || entity.is_a?(::String)
+              klass.valid?(attribute => entity)
+            else
+              klass.valid?(entity)
+            end
           end
+
           klass.errors.public_send(:store).data.each do |k,v|
-            errors.add([attribute, k.to_sym], v.pop)
+            if k.to_s == attribute.to_s
+              errors.add(attribute, v.pop)
+            else
+              errors.add([attribute, k.to_sym], v.pop)
+            end
           end
         else
           raise ::PoroValidator::InvalidValidator.new(
