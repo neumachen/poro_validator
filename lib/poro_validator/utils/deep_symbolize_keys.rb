1d837e1a678124bdb288354d93221454c409e243
Kareem Gan <kareemgan@gmail.com>
2016-01-22 11:20:25 -0600

    Add deep symbolizer utility

---
+++ lib/poro_validator/utils/deep_symbolize_keys.rb
@@ -0,0 +1,19 @@
+module PoroValidator
+  module Utils
+    module DeepSymbolizeKeys
+      class ::Hash
+        def deep_symbolize_keys
+          deep_transform_keys{ |key| key.to_sym rescue key }
+        end
+
+        def deep_transform_keys(&block)
+          result = {}
+          each do |key, value|
+            result[yield(key)] = value.is_a?(Hash) ? value.deep_transform_keys(&block) : value
+          end
+          result
+        end
+      end
+    end
+  end
+end
